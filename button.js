33518336f1cee77de046dc945ef64292251c699c
Lazar Stojanović <lakisd98@gmail.com>
2022-07-02 22:40:05 +0200

    Fix button functionality to redirect url

---
+++ button.js
@@ -0,0 +1,7 @@
+document.getElementById("secondSignUp").onclick = function () {
+    location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+};
+
+document.getElementById("SignUp").onclick = function () {
+    location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+};
