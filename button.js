606a9754ffe3b8ba5ebbb89b11e874952726c054
Lazar Stojanović <lakisd98@gmail.com>
2022-07-02 23:21:00 +0200

    Fix button javascript

--- button.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("secondSignUp").onclick = function () {
-    location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-};
-
-document.getElementById("SignUp").onclick = function () {
-    location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-};
